460fc8e5091f475e5950de478778629431cc5860
yy792e <73044159+yy889@users.noreply.github.com>
2026-07-13 22:59:32 +0800

    Delete b4543.kt

--- b4543.kt
+++
@@ -1,7 +0,0 @@
-package lgtm
-
-fun main(args: Array<String>) {
-    val (a,b,p,q,m) = readln().split(' ').map(String::toDouble)
-    println((a*p+b*q).toLong())
-    println((m-(a*p+b*q)).toLong())
-}
