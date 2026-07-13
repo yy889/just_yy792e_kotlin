bd12a8c90e2d0022a37a1735b80c6b1e779dc187
yy792e <73044159+yy889@users.noreply.github.com>
2026-07-13 23:01:51 +0800

    Delete p5712.kt

--- p5712.kt
+++
@@ -1,7 +0,0 @@
-package lgtm
-
-fun main(args : Array<String>) {
-    val input = readln().toInt()
-    if (input<=1) {println("Today, I ate $input apple.")}
-    else {println("Today, I ate $input apples.")}
-}
